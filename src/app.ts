83468989177595b664c76a66eabdfb8cd4ba1ac8
a1shuvo <a1shuvosaha@gmail.com>
2026-01-29 20:21:47 +0600

    Configured better auth with prisma

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
+import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import express, { Application } from "express";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(
   cors({
-    origin: process.env.APP_URL || "http://localhost:3000", // client side url
+    origin: process.env.APP_URL || "http://localhost:3000",
     credentials: true,
   }),
 );
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.json({ success: true, message: "SkillBridge Backend Running 🚀" });
 });
 
 export default app;
---
+++ src/lib/auth.ts
@@ -0,0 +1,38 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "STUDENT",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account consent",
+      accessType: "offline",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
