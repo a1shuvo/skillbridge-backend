db24704831a77786df867865245a820ff943cdb3
a1shuvo <a1shuvosaha@gmail.com>
2026-01-31 20:42:11 +0600

    Update admin validation script

--- src/modules/admin/admin.validation.ts
+++ src/modules/admin/admin.validation.ts
@@ -1,6 +1,10 @@
 import * as z from "zod";
 
 const updateUserStatusSchema = z.object({
+  params: z.object({
+    id: z.string().min(1, "User ID is required"),
+  }),
+
   body: z
     .object({
       status: z.enum(["ACTIVE", "BANNED"]).optional(),
